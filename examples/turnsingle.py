18d9b7b012540bf68f05b826e2ef7bcbe4e0a33f
Anshul Ramachandran <anshulr28@gmail.com>
2018-06-06 22:50:29 -0700

    uh

--- examples/turnsingle.py
+++ examples/turnsingle.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
     print "integer steps 200 = 360 dgs"
 
     pR.goto(200)
-    pR.goto(0)
-
-    print "float means angle dgs"
-
-    for i in range(9):
-        p.goto(i*45.0)
-        p.goto(0)
+    # pR.goto(0)
+    #
+    # print "float means angle dgs"
+    #
+    # for i in range(9):
+    #     p.goto(i*45.0)
+    #     p.goto(0)
