8297e21f1a5d977d04ce19b27457b5e3954f0d5c
Anshul Ramachandran <anshulr28@gmail.com>
2018-06-06 23:23:29 -0700

    uh

--- examples/turn.py
+++ examples/turn.py
@@ -1,16 +1,18 @@
 import pololu.pololu as pololu
+from multiprocessing import Pool
+
+def run(pol):
+    pol.speed = 16
+    pol.stepsleft(200)
+    pol.stepsright(200)
 
 if __name__ == '__main__':
 
     pR = pololu.Pololu(pololu.Pins(enable=17, direction=22, step=27))
     pL = pololu.Pololu(pololu.Pins(enable=5, direction=13, step=6))
 
-    pL.speed = 16
-    pL.stepsleft(400)
-    pL.stepsright(400)
-
-    pR.speed = 16
-    pR.stepsleft(400)
-    pR.stepsright(400)
+    motors = [pR, pL]
+    p = Pool()
+    p.map(run, motors)
 
     print "integer steps 200 = 360 dgs"
