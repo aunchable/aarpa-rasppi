6c462e2ab3caf285020faa9a27352bae2ba44d0e
Anshul Ramachandran <anshulr28@gmail.com>
2018-06-06 22:31:23 -0700

    two motor

--- examples/turn.py
+++ examples/turn.py
@@ -2,19 +2,26 @@ import pololu.pololu as pololu
 
 if __name__ == '__main__':
 
-    p = pololu.Pololu(pololu.Pins(enable=22, direction=17, step=27))
+    pR = pololu.Pololu(pololu.Pins(enable=11, direction=13, step=15))
+    pL = pololu.Pololu(pololu.Pins(enable=29, direction=31, step=33))
 
-    p.speed = 16
-    p.stepsleft(400)
-    p.stepsright(400)
+    pL.speed = 16
+    pL.stepsleft(400)
+    pL.stepsright(400)
+
+    pR.speed = 16
+    pR.stepsleft(400)
+    pR.stepsright(400)
 
     print "integer steps 200 = 360 dgs"
 
-    p.goto(200)
-    p.goto(0)
+    pL.goto(200)
+    pR.goto(200)
+    pL.goto(0)
+    pR.goto(0)
 
-    print "float means angle dgs"
-
-    for i in range(9):
-        p.goto(i*45.0)
-        p.goto(0)
+    # print "float means angle dgs"
+    #
+    # for i in range(9):
+    #     p.goto(i*45.0)
+    #     p.goto(0)
